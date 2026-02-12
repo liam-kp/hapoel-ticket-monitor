29e373db0390fddb366776c73e183f2d9c3546a9
liam-kp <liranmiller@gmail.com>
2026-02-13 06:33:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 console.log("START");
-
-(async () => {
-  try {
-    console.log("ENV CHECK", {
-      WHATSAPP_TOKEN: !!process.env.WHATSAPP_TOKEN,
-      PHONE_NUMBER_ID: process.env.PHONE_NUMBER_ID,
-      TO_PHONE: process.env.TO_PHONE
-    });
-
-    console.log("DONE");
-    process.exit(0);
-  } catch (e) {
-    console.error("CRASH", e);
-    process.exit(1);
-  }
-})();
+process.exit(0);
